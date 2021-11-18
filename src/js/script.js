f5003a68f0cdb021f02e8a9c400c32fc6254939a
Nazarii <nazariikubik@gmail.com>
2021-11-18 20:55:42 +0200

    Update nav and header complete all queries

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,24 @@ menuBtn.addEventListener("click", () => {
     menuOpen = false;
   }
 });
+
+// document.addEventListener("click", (e) => {
+//   let target = e.target;
+//   console.log(target.id);
+//   if (target.id === "check") {
+//     menuBtn.addEventListener("click", () => {
+//       if (!menuOpen) {
+//         content.style.cssText = "display: inline-block";
+//         menuOpen = true;
+//       } else {
+//         content.style.removeProperty("display");
+
+//         menuOpen = false;
+//       }
+//     });
+//   } else {
+//     content.style.removeProperty("display");
+//     menuBtn.checked = ;
+//     menuOpen = false;
+//   }
+// });
