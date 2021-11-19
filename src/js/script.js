4170fae99f9e2e36a7c195ce754955bfcef97d20
Nazarii <nazariikubik@gmail.com>
2021-11-19 14:34:21 +0200

    Javascript hamburger and transition

--- src/js/script.js
+++ src/js/script.js
@@ -3,34 +3,28 @@ const menuBtn = document.getElementById("check");
 
 let menuOpen = false;
 
-menuBtn.addEventListener("click", () => {
-  if (!menuOpen) {
-    content.style.cssText = "display: inline-block";
-    menuOpen = true;
+document.addEventListener("click", (e) => {
+  let target = e.target;
+
+  if (
+    target.className.toLowerCase() === "check" ||
+    target.className.toLowerCase() === "nav__hamburger" ||
+    target.className.toLowerCase() === "check-span"
+  ) {
+    menuBtn.addEventListener("click", () => {
+      if (!menuOpen) {
+        content.style.cssText = "display: inline-block";
+        menuOpen = true;
+      } else {
+        content.style.removeProperty("display");
+
+        menuOpen = false;
+      }
+    });
   } else {
     content.style.removeProperty("display");
+    menuBtn.checked = false;
 
     menuOpen = false;
   }
 });
-
-// document.addEventListener("click", (e) => {
-//   let target = e.target;
-//   console.log(target.id);
-//   if (target.id === "check") {
-//     menuBtn.addEventListener("click", () => {
-//       if (!menuOpen) {
-//         content.style.cssText = "display: inline-block";
-//         menuOpen = true;
-//       } else {
-//         content.style.removeProperty("display");
-
-//         menuOpen = false;
-//       }
-//     });
-//   } else {
-//     content.style.removeProperty("display");
-//     menuBtn.checked = ;
-//     menuOpen = false;
-//   }
-// });
